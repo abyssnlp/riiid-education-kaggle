10992612c3f2c3a5454e84c4b664dfe2f4d83af6
certainlie <shaurya.rawat@voicemod.net>
2020-12-07 23:03:27 +0100

    Start EDA

---
+++ initial_EDA.py
@@ -0,0 +1,11 @@
+# Initial EDA for Riiid Education Challenge
+# Author: Shaurya
+
+import pandas as pd
+import dask.dataframe as dd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+
+# Read in train parquet
