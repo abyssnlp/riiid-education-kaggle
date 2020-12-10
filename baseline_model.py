1eb91a2c0284932467cc883b777f56e96c6895f0
abyssnlp <rawatshaurya1994@gmail.com>
2020-12-10 23:10:43 +0100

    WIP

--- baseline_model.py
+++ baseline_model.py
@@ -11,6 +11,11 @@ import os
 import itertools
 from collections import Counter
 from dask.distributed import Client,progress
+from dask_ml.wrappers import ParallelPostFit
+from sklearn.model_selection import train_test_split
+from dask_ml.model_selection import train_test_split as dask_split
+import dask
+import dask_xgboost
 
 #? Dask client and workers
 client=Client(n_workers=4,threads_per_worker=2,memory_limit='2GB')
@@ -29,7 +34,27 @@ train.columns
 questions.columns
 lectures.columns
 
-# Prepare dataset for modeling
-train['timestamp'].describe().compute()
-train['timestamp_bin']=pd.qcut(train['timestamp'],4,labels=[1,2,3,4])
-train['timestamp_bin']=dd.map_partitions(pd.qcut(train['timestamp'],4,labels=[1,2,3,4]))
+# Run dataset raw on train
+# might need to standardscaler and remove nulls
+train['answered_correctly'].value_counts().compute()
+# remove -1s for lectures
+train=train[train['answered_correctly']!=-1]
+train=train.dropna()
+
+X=train[['timestamp','user_id','content_id','content_type_id','task_container_id','user_answer','prior_question_elapsed_time','prior_question_had_explanation']]
+y=train['answered_correctly']
+x_train,x_test,y_train,y_test=dask_split(X,y,test_size=0.2)
+
+x_train.shape
+y_train.shape
+
+# xgboost params
+params={
+    'objective':'binary:logistic',
+    'max_depth':4,
+    'eta':0.01,
+    'subsample':0.5,
+    'min_child_weight':0.5
+}
+
+result=dask_xgboost.train(client,params,x_train,y_train,num_boost_round=10)
